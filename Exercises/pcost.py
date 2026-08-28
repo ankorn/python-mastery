82e77799b0341285274f521bee0e65b6c5bb8035
ant.korneev <ant.korneev@tinkoff.ru>
2026-08-28 11:55:07 +0400

    1-4

---
+++ Exercises/pcost.py
@@ -0,0 +1,15 @@
+def portfolio_cost (fname):
+    sum = 0
+    
+    with open(fname, 'r', encoding='utf-8') as file:
+        for line in file:
+            line = line.split()
+            amount = line[1]
+            price = line[2]
+            
+            try:
+                sum += (float(amount) * float(price))
+            except ValueError as e:
+                print(f'Couldn\'t parse: {line}: {e}')
+            
+    return sum
