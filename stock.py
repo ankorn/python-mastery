51e8cfb210c5fdf38e68414e1c9aa75bd072c9e3
ant.korneev <ant.korneev@tinkoff.ru>
2026-08-31 17:38:40 +0400

    up to 3.4

--- stock.py
+++ stock.py
@@ -1,24 +1,52 @@
 import csv
 
 class Stock:
-    types = (str, int, float)
+    _types = (str, int, float)
+    __slots__ = '_shares', '_price', 'name'
     
     def __init__(self, name, shares, price):
         self.name = name
-        self.shares = shares
-        self.price = price
+        self._shares = shares
+        self._price = price
         
     @classmethod
     def from_row(cls, row: list[str]):
-        values = [func(value) for value, func in zip(row, cls.types)]
+        values = [func(value) for value, func in zip(row, cls._types)]
         
         return cls(*values)
         
+    @property
     def cost(self):
-        return self.shares * self.price
+        return self._shares * self._price
+    
+    @property
+    def shares(self):
+        return self._shares
+    @shares.setter
+    def shares(self, v):
+        if not isinstance(v, self._types[1]):
+            raise TypeError(f'Expected {self._types[1].__name__}')
+        
+        if v < 0:
+            raise ValueError('shares must be >= 0')
+        
+        self._shares = v
+        
+    @property
+    def price(self):
+        return self._price
+    @price.setter
+    def price(self, v):
+        if not isinstance(v, self._types[2]):
+            raise TypeError(f'Expected {self._types[2].__name__}')
+        
+        if v < 0:
+            raise ValueError('price must be >= 0')
+        
+        self._price = v
     
     def sell(self, nshares):
-        self.shares -= nshares
+        self._shares -= nshares
         
 def print_portfolio(portfolio):
     headings = vars(portfolio[0]).keys()
@@ -35,4 +63,4 @@ def print_portfolio(portfolio):
         
 from decimal import Decimal
 class DStock(Stock):
-    types = (str, int, Decimal)
+    _types = (str, int, Decimal)
