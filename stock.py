f855c70f835853bb5cd3185f128a761bfb79ad52
ant.korneev <ant.korneev@tinkoff.ru>
2026-09-07 11:04:08 +0400

    4.4

--- stock.py
+++ stock.py
@@ -101,6 +101,17 @@ class Spam:
         print('Spam.a')
     def b(self):
         print('Spam.b')
+        
+class MySpam:
+    def __init__(self):
+        self._spam = Spam()
+    def a(self):
+        print('MySpam.a')
+        self._spam.a()
+    def c(self):
+        print('MySpam.c')
+    def __getattr__(self, name):
+        return getattr(self._spam, name)
 
 a = SimpleStock('GOOG', 100, 490.10)
 a.shares = 75
