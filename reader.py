390cac8662d7f5bae7eb5efafe905639b8d74b77
ant.korneev <ant.korneev@tinkoff.ru>
2026-09-09 10:41:35 +0400

    5.5

--- reader.py
+++ reader.py
@@ -2,6 +2,7 @@
 import csv
 from collections.abc import Iterable, Callable
 from abc import ABC, abstractmethod
+import logging
 
 type Lines = Iterable[tuple[str, int, float]]
 type Types = list[Callable[[str], str | int | float]]
@@ -12,12 +13,33 @@ class ClassWithFromRow(ABC):
     def from_row(self, row: list[str]) -> Record:
         ...
         
-def convert_csv(lines, convert: Callable[[list[str], list[str, int, float]]], *, headers=None):
+log = logging.getLogger(__name__)
+        
+def convert_csv(lines, convert: Callable[[list[str], list[str, int, float]]], types: Types, *, headers=None):
     rows = csv.reader(lines)
     if headers is None:
         headers = next(rows)
+    
+    records = []
+    for i, row in enumerate(rows):
+        error = False
+        message = None
+        for t, v in zip(types, row):
+            try:
+                t(v)
+            except ValueError as e:
+                message = e
+                error = True
+                
+        if error:
+            log.warning(f'Row {i + 1}: Bad row: {row}')
+            
+            if message:
+                log.debug(f'Row {i + 1}: Reason: {message}')
+        else:
+            records.append(convert(headers, row))
         
-    return list(map(lambda row: convert(headers, row), rows))
+    return records
 
 def csv_as_dicts(lines: Lines, types: Types, *, headers=None) -> Records:
     '''
@@ -26,17 +48,17 @@ def csv_as_dicts(lines: Lines, types: Types, *, headers=None) -> Records:
     def make_typed_dict(headers, row):
         return { name: func(value) for name, value, func in zip(headers, row, types) }
     
-    records = convert_csv(lines, make_typed_dict)
+    records = convert_csv(lines, make_typed_dict, types)
     return records
 
-def csv_as_instances(lines: Lines, cls: ClassWithFromRow, *, headers=None) -> Records:
+def csv_as_instances(lines: Lines, cls: ClassWithFromRow, types: Types, *, headers=None) -> Records:
     '''
     Convert lines of CSV data into a list of instances
     '''
     def make_instance(_, row):
         return cls.from_row(row)
     
-    records = convert_csv(lines, make_instance)
+    records = convert_csv(lines, make_instance, types)
     return records
 
 def read_csv_as_dicts(filename, types, *, headers=None):
@@ -52,26 +74,3 @@ def read_csv_as_instances(filename, cls, *, headers=None):
     '''
     with open(filename) as file:
         return csv_as_instances(file, cls, headers=headers)
-    
-    
-def counter(value):
-    def incr():
-        nonlocal value
-        value += 1
-        return value
-
-    def decr():
-        nonlocal value
-        value -= 1
-        return value
-
-    return incr, decr
-up, down = counter(0)
-r = up()
-r = up()
-r = up()
-r = up()
-r = up()
-
-
-print(r)
